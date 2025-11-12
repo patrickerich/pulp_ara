bfb97497a8942e50275f648df4a9a2a9bd8aacca
Patrick Erich <git@patrickerich.email>
2025-11-12 14:03:25 +0100

    Add VCD dump format option for xcelium based sims

--- hardware/scripts/xcelium.tcl
+++ hardware/scripts/xcelium.tcl
@@ -1,18 +1,29 @@
-# Copyright 2021 ETH Zurich and University of Bologna.
-# Solderpad Hardware License, Version 0.51, see LICENSE for details.
-# SPDX-License-Identifier: SHL-0.51
-#
 # Xcelium-specific simulation script
 
 # Check if trace environment variable is set for waveform dumping
 if {[info exists ::env(XCELIUM_TRACE)] && $::env(XCELIUM_TRACE) == "1"} {
-    # Open SHM database for waveform dumping
-    database -open xcelium_waves -shm -default
-    # Probe all signals at all levels
-    probe -create -all -depth all -database xcelium_waves
-    puts "Xcelium: Waveform tracing enabled - xcelium_waves.shm"
+    # Select waveform format: default SHM, optional VCD via XCELIUM_WAVE_FORMAT=vcd
+    set fmt "shm"
+    if {[info exists ::env(XCELIUM_WAVE_FORMAT)]} {
+        set fmt [string tolower $::env(XCELIUM_WAVE_FORMAT)]
+    }
+    set dbname "xcelium_waves"
+    if {$fmt eq "vcd"} {
+        # Open VCD database/file
+        set vcdfile "xcelium_waves.vcd"
+        database -open $dbname -vcd -into $vcdfile -default
+        # Probe all signals at all levels
+        probe -create -all -depth all -database $dbname
+        puts "Xcelium: Waveform tracing enabled - $vcdfile (VCD)"
+    } else {
+        # Open SHM database for waveform dumping
+        database -open $dbname -shm -default
+        # Probe all signals at all levels
+        probe -create -all -depth all -database $dbname
+        puts "Xcelium: Waveform tracing enabled - $dbname.shm (SHM)"
+    }
 } else {
-    puts "Xcelium: Running without waveform tracing (use trace=1 to enable)"
+    puts "Xcelium: Running without waveform tracing (use trace=1 to enable; optional wavefmt=vcd|shm)"
 }
 
 # Run simulation to completion
